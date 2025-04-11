d4edba089e174c0a4e31842746550fdeef6fbcd1
vivy_phtela <tubasa.0405@icloud.com>
2025-04-11 23:45:54 +0900

    fix: フッターの修正

--- src/features/common/footer/components/Footer.tsx
+++ src/features/common/footer/components/Footer.tsx
@@ -1,8 +1,34 @@
+"use client";
+
+import Link from "next/link";
+import { FaFacebook, FaGithub } from "react-icons/fa";
+
 export default function Footer() {
   return (
-    <footer className="flex flex-col items-center justify-center w-full h-24 bg-gray-800 text-white">
-      <div className="text-sm">© 2025 NishidaLab</div>
-      <div className="text-sm">All rights reserved.</div>
+    <footer className="bg-gray-800 text-white w-full p-6">
+      <div className="flex items-center justify-center gap-6">
+        <p className="text-xs md:text-sm text-center">
+          © 2025 NishidaLab. All rights reserved.
+        </p>
+        <div className="flex gap-3">
+          <Link
+            href="https://github.com/kitaq-nishidalab"
+            target="_blank"
+            aria-label="GitHub"
+            className="hover:text-gray-400"
+          >
+            <FaGithub size={28} />
+          </Link>
+          <Link
+            href="https://www.facebook.com/NishidaTakeshiLab"
+            target="_blank"
+            aria-label="Facebook"
+            className="hover:text-gray-400"
+          >
+            <FaFacebook size={28} />
+          </Link>
+        </div>
+      </div>
     </footer>
   );
 }
