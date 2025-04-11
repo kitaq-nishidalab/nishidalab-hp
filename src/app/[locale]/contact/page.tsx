243ac723141a6e0e683f3982448e3ec7bb98d50e
vivy_phtela <tubasa.0405@icloud.com>
2025-04-12 01:40:19 +0900

    fix: 文字サイズ修正

--- src/app/[locale]/contact/page.tsx
+++ src/app/[locale]/contact/page.tsx
@@ -33,7 +33,7 @@ export default async function ContactPage({
       <div className="p-8 md:p-16">
         <div className="space-y-5 mb-16">
           <CenteredTextWithUnderline text={t("contact.contact1")} />
-          <p className="text-lg">
+          <p className="text-sm md:text-lg">
             <a
               href={`mailto:${t("contact.email")}`}
               className="text-blue-500 underline"
@@ -44,7 +44,7 @@ export default async function ContactPage({
         </div>
         <div className="space-y-5 mb-16">
           <CenteredTextWithUnderline text={t("contact.contact2")} />
-          <p className="text-lg">
+          <p className="text-sm md:text-lg">
             <a
               href={`mailto:${t("contact.email")}`}
               className="text-blue-500 underline"
@@ -55,7 +55,7 @@ export default async function ContactPage({
         </div>
         <div className="space-y-5">
           <CenteredTextWithUnderline text={t("contact.access")} />
-          <p className="text-lg">{t("contact.address")}</p>
+          <p className="text-sm md:text-lg">{t("contact.address")}</p>
           <iframe
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3312.0062035265823!2d130.70962887585353!3d33.88949407321888!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3543cbd8d8d13b4d%3A0x97af4e3ff45c8c58!2z5YyX5Lmd5bee5biC56uL5aSn5a2mIOOBsuOBs-OBjeOBruOCreODo-ODs-ODkeOCuQ!5e0!3m2!1sja!2sjp!4v1744264765560!5m2!1sja!2sjp"
             width="100%"
--- src/components/CenteredTextWithUnderline.tsx
+++ src/components/CenteredTextWithUnderline.tsx
@@ -2,7 +2,7 @@ export default function CenteredTextWithUnderline({ text }: { text: string }) {
   return (
     <div className="text-center">
       <div className="inline-block px-4">
-        <span className="text-2xl">{text}</span>
+        <span className="text-lg:text-2xl">{text}</span>
       </div>
       <div className="mt-2 mb-8 border-t-2 border-gray-300" />
     </div>
